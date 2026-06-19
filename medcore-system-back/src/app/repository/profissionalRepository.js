e5819ca4650fdfac321756b169564f4c9b4fbc07
AUGUSTO BENJAMIN CHUPERNATE <augusto.7959@aluno.pr.senac.br>
2026-06-19 19:58:54 -0300

    fix(back): corrige código de profissional

--- medcore-system-back/src/app/repository/profissionalRepository.js
+++ medcore-system-back/src/app/repository/profissionalRepository.js
@@ -11,13 +11,13 @@ const findById = async (id) => {
 }
 
 const create =  async ({nome, registro, especialidade_id, cargo, turno, telefone, email, ativo}) => {
-    const result = await pool.query("insert into profissionais (nome, registro, especialidade_id, cargo, turno, telefone, email, ativo) values ($1, $2, $N, $4, $5, $6, $7, $8) returning *", [nome, registro, especialidade_id, cargo, turno, telefone, email, ativo])
+    const result = await pool.query("insert into profissionais (nome, registro, especialidade_id, cargo, turno, telefone, email, ativo) values ($1, $2, $3, $4, $5, $6, $7, $8) returning *", [nome, registro, especialidade_id, cargo, turno, telefone, email, ativo])
 
     return result.rows[0]
 }
 
 const update = async (id, {nome, registro, especialidade_id, cargo, turno, telefone, email, ativo}) => {
-    const result = await pool.query("update profissionais set nome=$1, registro=$2, especialidade_id=$N, cargo=$4, turno=$5, telefone=$6, email=$7, ativo=$8 where id = $9 returning *", [nome, registro, especialidade_id, cargo, turno, telefone, email, ativo, id])
+    const result = await pool.query("update profissionais set nome=$1, registro=$2, especialidade_id=$3, cargo=$4, turno=$5, telefone=$6, email=$7, ativo=$8 where id = $9 returning *", [nome, registro, especialidade_id, cargo, turno, telefone, email, ativo, id])
 
     return result.rows[0]
 }
