aca1342223f8bde03b8b70d9eb57f25f04e472f5
Augusto <98566110+gutoBJ@users.noreply.github.com>
2026-06-24 11:46:05 -0300

    fix(back): melhora cors

--- medcore-system-back/src/index.js
+++ medcore-system-back/src/index.js
@@ -3,12 +3,14 @@ const cors = require('cors')
 require('dotenv').config()
 
 const router = require('./routes')
-require('./app/db') // inicializa a conexão ao subir o servidor
+require('./app/db')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({
+  origin: '*' // permite qualquer origem — ok para trabalho acadêmico
+}))
 app.use(express.json())
 
 app.use('/api', router)
